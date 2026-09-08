73daa1d83b263e8f9f83791a877ce708d90abb38
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-09-08 15:49:07 +0100

    Testing account creation and connection via userService

---
+++ src/main/java/ma/youcode/lineperm/Main.java
@@ -0,0 +1,29 @@
+package ma.youcode.lineperm;
+
+import ma.youcode.lineperm.model.User;
+import ma.youcode.lineperm.service.UserService;
+
+public class Main {
+    public static void main(String[] args) {
+        UserService userService = new UserService();
+        userService.charger();
+        String login = "mohamed";
+        String motDePasse = "123456";
+
+        if (userService.creerCompte(login, motDePasse)) {
+            System.out.println("Compte cree avec succes !");
+        } else {
+            System.out.println("Le compte existe deja.");
+        }
+
+        User user = userService.connecter(login, motDePasse);
+        if (user != null) {
+            System.out.println("Connexion reussie");
+
+            System.out.println("Bienvenue " + user.getLogin());
+        } else {
+            System.out.println("Login ou mot de pass incorrectes.");
+        }
+
+    }
+}
