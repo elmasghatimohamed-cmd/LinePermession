0efa892a8c90b00e67afdb673fde6b64e116285c
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-09-10 14:24:46 +0100

    Adding getOthersBloc and towardsLine methods

--- src/main/java/ma/youcode/lineperm/model/FichierProtege.java
+++ src/main/java/ma/youcode/lineperm/model/FichierProtege.java
@@ -13,8 +13,8 @@ public class FichierProtege {
     private boolean otherD;
 
     public FichierProtege(String nom, String proprietaire,
-                           boolean ownerR, boolean ownerW, boolean ownerD,
-                           boolean otherR, boolean otherW, boolean otherD) {
+            boolean ownerR, boolean ownerW, boolean ownerD,
+            boolean otherR, boolean otherW, boolean otherD) {
         this.nom = nom;
         this.proprietaire = proprietaire;
         this.ownerR = ownerR;
@@ -29,36 +29,51 @@ public class FichierProtege {
         this(nom, proprietaire, true, true, true, false, false, false);
     }
 
-    public boolean isOwnerR() { 
-        return ownerR; 
-    }
-    public boolean isOwnerW() { 
-        return ownerW; 
-    }
-    public boolean isOwnerD() { 
-        return ownerD; 
-    }
-    public boolean isOtherR() { 
-        return otherR; 
-    }
-    public boolean isOtherW() { 
-        return otherW; 
-    }
-    public boolean isOtherD() { 
-        return otherD; 
+    public boolean isOwnerR() {
+        return ownerR;
     }
 
-    public void setOtherR(boolean value) { 
-        this.otherR = value; 
+    public boolean isOwnerW() {
+        return ownerW;
     }
-    public void setOtherW(boolean value) { 
-        this.otherW = value; 
+
+    public boolean isOwnerD() {
+        return ownerD;
     }
-    public void setOtherD(boolean value) { 
-        this.otherD = value; 
+
+    public boolean isOtherR() {
+        return otherR;
     }
- 
+
+    public boolean isOtherW() {
+        return otherW;
+    }
+
+    public boolean isOtherD() {
+        return otherD;
+    }
+
+    public void setOtherR(boolean value) {
+        this.otherR = value;
+    }
+
+    public void setOtherW(boolean value) {
+        this.otherW = value;
+    }
+
+    public void setOtherD(boolean value) {
+        this.otherD = value;
+    }
+
     public String getBlocProprietaire() {
         return (ownerR ? "r" : "-") + (ownerW ? "w" : "-") + (ownerD ? "d" : "-");
+    }
+
+    public String getBlocAutres() {
+        return (otherR ? "r" : "-") + (otherW ? "w" : "-") + (otherD ? "d" : "-");
+    }
+
+    public String versLigne() {
+        return nom + ";" + proprietaire + ";" + getBlocProprietaire() + ";" + getBlocAutres();
     }
 }
