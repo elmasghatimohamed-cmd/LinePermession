48ab4f681a328aa07d3c4a9e69d39486f2eb4285
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-09-11 11:09:40 +0100

    Update main classe to test the permission

--- src/main/java/ma/youcode/lineperm/Main.java
+++ src/main/java/ma/youcode/lineperm/Main.java
@@ -14,42 +14,42 @@ public class Main {
 
         User user = new User("Mohamed", "123456");
 
-        System.out.println("\n========== AVANT CREATION ==========");
-        for (FichierProtege fichier : fileService.listerTous()) {
-            System.out.println(fichier.getNom());
-        }
+        // System.out.println("\n========== AVANT CREATION ==========");
+        // for (FichierProtege fichier : fileService.listerTous()) {
+        // System.out.println(fichier.getNom());
+        // }
 
-        System.out.println("\n========== CREATION ==========");
+        // System.out.println("\n========== CREATION ==========");
 
-        boolean resultat = fileService.creerFichier(user, "instant.txt");
+        // boolean resultat = fileService.creerFichier(user, "instant.txt");
 
-        System.out.println(
-                resultat
-                        ? "Nouveau fichier cree avec succes."
-                        : "Echec de creation.");
+        // System.out.println(
+        // resultat
+        // ? "Nouveau fichier cree avec succes."
+        // : "Echec de creation.");
 
-        System.out.println("\n========== APRES CREATION ==========");
+        // System.out.println("\n========== APRES CREATION ==========");
 
-        for (FichierProtege fichier : fileService.listerTous()) {
-            System.out.println(
-                    fichier.getNom()
-                            + " | Proprietaire: "
-                            + fichier.getProprietaire()
-                            + " | Droits: "
-                            + fichier.getBlocProprietaire()
-                            + "|" + fichier.getBlocAutres());
-        }
+        // for (FichierProtege fichier : fileService.listerTous()) {
+        // System.out.println(
+        // fichier.getNom()
+        // + " | Proprietaire: "
+        // + fichier.getProprietaire()
+        // + " | Droits: "
+        // + fichier.getBlocProprietaire()
+        // + "|" + fichier.getBlocAutres());
+        // }
 
         String nomFichier = "file.txt";
-        String contenu = "Hello world";
+        // String contenu = "Hello world";
 
-        boolean ecriture = fileService.ecrireContenu(user, nomFichier, contenu);
+        // boolean ecriture = fileService.ecrireContenu(user, nomFichier, contenu);
 
-        if (ecriture) {
-            System.out.println("Contenu ecrit avec succes");
-        } else {
-            System.out.println("Echec de l'ecriture");
-        }
+        // if (ecriture) {
+        // System.out.println("Contenu ecrit avec succes");
+        // } else {
+        // System.out.println("Echec de l'ecriture");
+        // }
 
         String contenuLu = fileService.lireContenu(user, nomFichier);
 
@@ -59,6 +59,14 @@ public class Main {
         } else {
             System.out.println("Fichier introuvable");
         }
+
+        FichierProtege fichier = fileService.listerTous().iterator().next();
+
+        System.out.println("Avant : " + fichier.getBlocAutres());
+
+        fileService.modifierDroits(user, fichier.getNom(), "-rw");
+
+        System.out.println("Apres +rw : " + fichier.getBlocAutres());
 
     }
 
