d2eec77f6e2a6455cee4b12e49229369ee04494b
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-09-15 12:28:56 +0100

    Test LogAnalyzer functionalities in Main class

--- src/main/java/ma/youcode/lineperm/Main.java
+++ src/main/java/ma/youcode/lineperm/Main.java
@@ -1,12 +1,39 @@
 package ma.youcode.lineperm;
 
-import ma.youcode.lineperm.ui.ConsoleApp;
+import java.util.Arrays;
+import java.util.List;
+
+import ma.youcode.lineperm.log.LogAnalyzer;
+import ma.youcode.lineperm.model.AccessLog;
+// import ma.youcode.lineperm.ui.ConsoleApp;
 
 public class Main {
     public static void main(String[] args) {
-        ConsoleApp app = new ConsoleApp();
-        app.demarrer();
+        // ConsoleApp app = new ConsoleApp();
+        // app.demarrer();
 
+
+        List<AccessLog> logs = Arrays.asList(
+                new AccessLog("2026-09-15", "10:00", "Mohamed", "LECTURE", "test.txt", "AUTORISE"),
+                new AccessLog("2026-09-15", "10:05", "Mohamed", "LECTURE", "test.txt", "REFUSE"),
+                new AccessLog("2026-09-15", "10:10", "Ali", "LECTURE", "test.txt", "AUTORISE"),
+                new AccessLog("2026-09-15", "10:15", "Ali", "LECTURE", "data.txt", "AUTORISE"),
+                new AccessLog("2026-09-15", "10:20", "Sara", "LECTURE", "data.txt", "REFUSE"),
+                new AccessLog("2026-09-15", "10:25", "Mohamed", "LECTURE", "data.txt", "AUTORISE"),
+                new AccessLog("2026-09-15", "10:30", "Ali", "LECTURE", "test.txt", "AUTORISE")
+        );
+
+        LogAnalyzer analyzer = new LogAnalyzer(logs);
+
+        System.out.println("Nombre total d'actions : "+ analyzer.nbrTotaleAction());
+
+        System.out.println("Nombre d'acces refuses : "+ analyzer.nombreAccesRefuses());
+
+        System.out.println("Utilisateurs distincts : "+ analyzer.utilisateursDistinct());
+
+        System.out.println("Actions par utilisateur : "+ analyzer.actionParUtilisateur());
+
+        System.out.println("Top 3 fichiers consultes : "+ analyzer.topFichierConsultes());
     }
 
 }
