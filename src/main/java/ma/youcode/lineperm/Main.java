6ac66eaf539370c9ac221a3d04b4dddca7132361
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-09-16 15:52:49 +0100

    Refactor Main class to utilize ConsoleApp for application startup

--- src/main/java/ma/youcode/lineperm/Main.java
+++ src/main/java/ma/youcode/lineperm/Main.java
@@ -1,55 +1,11 @@
 package ma.youcode.lineperm;
 
-import java.util.Arrays;
-import java.util.List;
-
-import ma.youcode.lineperm.log.LogAnalyzer;
-import ma.youcode.lineperm.model.AccessLog;
-import ma.youcode.lineperm.log.LogService;
-// import ma.youcode.lineperm.ui.ConsoleApp;
+import ma.youcode.lineperm.ui.ConsoleApp;
 
 public class Main {
     public static void main(String[] args) {
-        // ConsoleApp app = new ConsoleApp();
-        // app.demarrer();
-
-
-        List<AccessLog> logss = Arrays.asList(
-                new AccessLog("2026-09-15", "10:00", "Mohamed", "LECTURE", "test.txt", "AUTORISE"),
-                new AccessLog("2026-09-15", "10:05", "Mohamed", "LECTURE", "test.txt", "REFUSE"),
-                new AccessLog("2026-09-15", "10:10", "Ali", "Ecriture", "test.txt", "AUTORISE"),
-                new AccessLog("2026-09-15", "10:15", "Ali", "LECTURE", "data.txt", "AUTORISE"),
-                new AccessLog("2026-09-15", "10:20", "Sara", "Suppression", "data.txt", "REFUSE"),
-                new AccessLog("2026-09-15", "10:25", "Mohamed", "LECTURE", "data.txt", "AUTORISE"),
-                new AccessLog("2026-09-15", "10:30", "Ali", "LECTURE", "test.txt", "AUTORISE")
-        );
-
-        LogAnalyzer analyzer = new LogAnalyzer(logss);
-
-        System.out.println("Nombre total d'actions : "+ analyzer.nbrTotaleAction());
-
-        System.out.println("Nombre d'acces refuses : "+ analyzer.nombreAccesRefuses());
-
-        System.out.println("Utilisateurs distincts : "+ analyzer.utilisateursDistinct());
-
-        System.out.println("Actions par utilisateur : "+ analyzer.actionParUtilisateur());
-
-        System.out.println("Top 3 fichiers consultes : "+ analyzer.topFichierConsultes());
-        System.out.println("Acces refuses pour Mohamed : "+ analyzer.accesRefusesUtilisateur("Mohamed"));
-
-        System.out.println("Acces refuses pour Ali : "+ analyzer.accesRefusesUtilisateur("Ali"));
-
-        System.out.println("Acces refuses pour Sara : "+ analyzer.accesRefusesUtilisateur("Sara"));
-        System.out.println("Utilisateur le plus actif : "+ analyzer.utilisateurPlusActif());
-        System.out.println("Repartition par actions : "+ analyzer.repartitionParAction());
-
-        LogService logService = new LogService();
-
-        List<AccessLog> logs = logService.chargerLogs();
-
-        System.out.println("Nombre de logs : " + logs.size());
-
-        logs.forEach(System.out::println);
+        ConsoleApp app = new ConsoleApp();
+        app.demarrer();
     }
 
 }
