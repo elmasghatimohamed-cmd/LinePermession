003f978b473784a823a8476f6e0d1cce00c8b9f4
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-09-21 16:03:25 +0000

    Add database connection functionality and initial SQLite setup

--- src/main/java/ma/youcode/lineperm/Main.java
+++ src/main/java/ma/youcode/lineperm/Main.java
@@ -1,11 +1,27 @@
 package ma.youcode.lineperm;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import ma.youcode.lineperm.db.DBConnection;
 import ma.youcode.lineperm.ui.ConsoleApp;
 
 public class Main {
     public static void main(String[] args) {
-        ConsoleApp app = new ConsoleApp();
-        app.demarrer();
+        // ConsoleApp app = new ConsoleApp();
+        // app.demarrer();
+
+        try {
+            Connection connection = DBConnection.getConnection();
+
+            if (connection != null && !connection.isClosed()) {
+                System.out.println("Connexion SQLite reussie");
+                System.out.println("Base de donnees : " + connection.getCatalog());
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Erreur de connexion a SQLite : " + e.getMessage());
+        }
     }
 
 }
