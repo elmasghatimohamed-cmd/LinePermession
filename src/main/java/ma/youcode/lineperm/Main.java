1324041da8bba2cc16d86df35dbe085fe5102101
elmasghatimohamed-cmd <elmasghatimohamed@gmail.com>
2026-09-22 13:20:55 +0000

    Test user creation and retrieval in Main class

--- src/main/java/ma/youcode/lineperm/Main.java
+++ src/main/java/ma/youcode/lineperm/Main.java
@@ -7,23 +7,37 @@ import ma.youcode.lineperm.db.DBConnection;
 import ma.youcode.lineperm.db.DatabaseInitializer;
 import ma.youcode.lineperm.ui.ConsoleApp;
 
+import ma.youcode.lineperm.dao.UserDao;
+import ma.youcode.lineperm.model.User;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // ConsoleApp app = new ConsoleApp();
         // app.demarrer();
 
-        try {
-            Connection connection = DBConnection.getConnection();
+        UserDao userDao = new UserDao();
 
-            if (connection != null && !connection.isClosed()) {
-                System.out.println("Connexion SQLite reussie");
-                System.out.println("Base de donnees : " + connection.getCatalog());
-            }
+        User user = new User(
+                "architect_test",
+                "hash_test");
 
-        } catch (SQLException e) {
-            System.out.println("Erreur de connexion a SQLite : " + e.getMessage());
+        userDao.save(user);
+
+        System.out.println("ID : " + user.getId());
+
+        User found = userDao.findByLogin("architect_test");
+
+        if (found != null) {
+            System.out.println("Login : " + found.getLogin());
+            System.out.println("Hash : " + found.getPasswordHash());
         }
-        DatabaseInitializer.init();
+
+        System.out.println("\n=== TOUS LES USERS ===");
+
+        for (User u : userDao.findAll()) {
+            System.out.println(
+                    u.getId() + " - " + u.getLogin());
+        }
     }
 
 }
